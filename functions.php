372a0b2709286afd9a58cad8d0902a5bbbc84826
Fedoua <fedoua.zaouay.osengo@gmail.com>
2026-07-06 13:56:07 +0200

    Ajout de wp_enqueue_style pour charger le CSS, correction du sélecteur .logo a

--- functions.php
+++ functions.php
@@ -6,3 +6,8 @@ function nathalie_mota_register_menus() {
     ));
 }
 add_action('after_setup_theme', 'nathalie_mota_register_menus');
+
+function nathalie_mota_enqueue_assets() {
+    wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'nathalie_mota_enqueue_assets');
